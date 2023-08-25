ec7fc4e9ea5d4636c2f3c3e46235adaa5ad6cef0
Jack <jacktran.9922@gmail.com>
2023-08-24 23:25:34 -0700

    Setup script that will create the two directories and sample files needed for library.sh to run

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Create necessary directories
+mkdir -p library checked_out
+
+# Create sample book files
+touch library/Book1.txt
+touch library/Book2.txt
+touch library/Book3.txt
+
+echo "Sample books and directories created successfully."
